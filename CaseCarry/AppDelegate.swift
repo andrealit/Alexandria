ba807184957154ac19da24b0f0c3592596ef64ee
Andrea Tongsak <atongsak@gmail.com>
2019-09-07 17:13:48 -0400

    Added map functionality

--- CaseCarry/AppDelegate.swift
+++ CaseCarry/AppDelegate.swift
@@ -13,7 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let stack = CoreDataStack(modelName: "CaseCarry")!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ CaseCarry/Map Folder/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  CaseCarry
+//
+//  Created by Andrea Tongsak on 9/7/19.
+//  Copyright © 2019 Andrea Tongsak. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    // MARK: IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
